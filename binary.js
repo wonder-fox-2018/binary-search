d7cb6edf57e8a1c9f3a2bf3b5fc186df9308a556
Hedya Febritanti <hedya.feb@gmail.com>
2018-09-04 23:08:27 +0700

    binary-search hedyafeb

--- binary.js
+++ binary.js
@@ -4,14 +4,51 @@ var testArrayGenap = [40, 18, 22, 32, 90, 10, 10, 22, 8]
 var testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55]
 
 function ownSort(arr) {
-  // Your sorting code
+  
   return arr
 }
 
-function binarySearch (search, array) {
-  // Your searching code
-  return 0;
+function binary_search (search, array) {  
+  var indexAwal = 0; // 0
+  var indexAkhir = array.length-1; // 6
+  var range = indexAkhir - indexAwal;
+  var indexTengah = Math.floor((range) / 2); // 3
+
+
+  while (indexTengah <= indexAkhir && indexTengah >= indexAwal) {
+    if (array[indexTengah] === search) { 
+      // console.log(array[midIndex]);
+      return indexTengah;
+    }
+    else if (search < array[indexTengah]) {
+      indexAkhir = indexTengah - 1;
+      indexTengah = Math.floor((indexAkhir - indexAwal) / 2) + indexAwal;
+    }
+    else if (search > array[indexTengah]) {
+      indexAwal = indexTengah + 1;
+      indexTengah = Math.floor((indexAkhir - indexAwal) / 2) + indexAwal;
+    }
+  }
+  // IF NOT FOUND:
+  return -1;
 }
+
+
+
+
+
+// TEST CASE:
+let test_array1 = [1,2,3,4,5]
+console.log(binary_search(4, test_array1) === 3); // true
+
+let test_array2 = [13,19,24,29,32,37,43]
+console.log(binary_search(35, test_array2) === -1); // true (testcase: 35)
+
+let test_array3 = [100,120,130,135,150,170]
+console.log(binary_search(135, test_array3) === 3); // true (testcase: 135)
+
+let test_array4 = [100,120,130,135,150,170]
+console.log(binary_search(150, test_array4) === 4); // true (testcase: 135)
 
 var arrayGenapSorted = ownSort(testArrayGenap)
 var arrayGanjilSorted = ownSort(testArrayGanjil)
@@ -28,3 +65,5 @@ console.log(binary_search(2, arrayGanjilSorted))
 module.exports = {
   binary_search
 }
+
+
