99a499d6d1d77dbecac3a46edfd38ac50329c50e
Your Name <hokandre@mhs.mdp.ac.id>
2018-09-04 21:43:33 +0700

    update binary

--- binary.js
+++ binary.js
@@ -6,10 +6,94 @@
 
   FUNCTION ownSort("arr"){
     SET "data" with value "arr"
-    LOOP through each number in "data"
-      SET 
-    End Loop 
+    LOOP through each number in "data" start from "i"=0
+      STORE "indexBefore" with value "i"-1
+      STORE "dataBefore" with value "data[indexBefore]"
+      IF "data[i]" less then "dataBefore" THEN
+        STORE "data[indexBefore]" with value "data[i]"
+        STORE "data[i]" with value "dataBefore"
+        WHILE "indexBefore" not equals 0 THEN
+          STORE "dataMundur" with value "data[indexBefore]"
+          IF "data[indexBefore]" less then "data[indexBefore-1]" THEN
+            STORE "data[indexBefore]" with value "data[indexBefore-1]"
+            STORE "data[indexBefore-1]" with value "dataMundur"
+          End IF
+          DECREMENT "indexBefore" by minus 1
+        END WHILE
+      END IF
+    End Loop
+    
+    RETURN "data"
   }
+
+FUNCTION binarySearch ("search", "array") {
+  SET newArray=array.slice(0)
+  // Your searching code
+  WHILE "newArray's length" not equals  0)
+    IF length of "newArray" modulus 2 equals 1 THEN
+      STORE "median" with length of "newArray" divided 2 and floored
+      IF "newArray[median]" equals "search" THEN
+        RETURN true
+      ELSE IF "search" greater then "newArray[median]" THEN
+        DELETE "newArray" value from 0 how many "median+1"
+      ELSE
+        DELETE "newArray" value from "median" until length of "newArray"
+      END IF
+    
+    ELSE
+      STORE "median" with value length of "newArray" divided 2
+      IF "newArray[median]" equals "search" THEN
+        RETURN true
+      ELSE IF "search" greater then "newArray[median]" THEN
+        DELETE "newArray" value from 0 how many "median+1"
+      ELSE
+        DELETE "newArray" value from "median" until length of "newArray"
+      END IF
+    END IF
+  END WHILE
+  
+  RETURN -1;
+}
+
+FUNCTION binarySearchRelease1 ("search","array"){
+  SET "arraySort" with value CALL "ownSort" with parameter "array"
+  SET "newArray" with value COPY value "arraySort"
+
+  SET "index" with value []
+  LOOP trough each number in "array" start from "i"=0
+    STORE "i" in "index"
+  END LOOP
+
+  WHILE "newArray's length" not equals  0)
+    IF length of "newArray" modulus 2 equals 1 THEN
+      STORE "median" with length of "newArray" divided 2 and floored
+      IF "newArray[median]" equals "search" THEN
+        RETURN "index[median]"
+      ELSE IF "search" greater then "newArray[median]" THEN
+        DELETE "newArray" value from 0 how many "median+1"
+        DELETE "index" value from 0 how many "median+1"
+      ELSE
+        DELETE "newArray" value from "median" until length of "newArray"
+        DELETE "index" value from "median" length of "newArray"
+      END IF
+    
+    ELSE
+      STORE "median" with value length of "newArray" divided 2
+      IF "newArray[median]" equals "search" THEN
+        RETURN "index[median]"
+      ELSE IF "search" greater then "newArray[median]" THEN
+        DELETE "newArray" value from 0 how many "median+1"
+        DELETE "index" value from 0 how many "median+1"
+      ELSE
+        DELETE "newArray" value from "median" until length of "newArray"
+        DELETE "index" value from "median" length of "newArray"
+      END IF
+    END IF
+  END WHILE
+  
+  RETURN -1;
+
+}
 */
 var testArrayGenap = [40, 18, 22, 32, 90, 10, 10, 22, 8]
 var testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55]
@@ -35,7 +119,7 @@ function ownSort(arr) {
   }
   return data
 }
-//mencari median
+
 function binarySearch (search, array) {
   var newArray=array.slice(0)
   // Your searching code
@@ -66,8 +150,9 @@ function binarySearch (search, array) {
     }
   }
   
-  return 0;
+  return -1;
 }
+/*mengembalikan indeks*/
 function binarySearchRelease1 (search, array) {
   var arraySort=ownSort(array)
   var newArray=arraySort.slice(0)
