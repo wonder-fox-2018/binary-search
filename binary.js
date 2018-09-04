749aea1b5aaa57ba461bb48ee03155db4c803e22
danyismail <dany_arie@yahoo.com>
2018-09-04 23:57:18 +0700

    belum selesai

--- binary.js
+++ binary.js
@@ -4,12 +4,13 @@ var testArrayGenap = [40, 18, 22, 32, 90, 10, 10, 22, 8]
 var testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55]
 
 function ownSort(arr) {
-  // Your sorting code
+  var mid = Math.floor(testArrayGenap.length/2) 
+
   return arr
 }
 
 function binarySearch (search, array) {
-  // Your searching code
+  
   return 0;
 }
 
